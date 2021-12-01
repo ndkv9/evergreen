8453ad7c859f8eb8551b2f26f0c5069e1d9d5a96
Vu Nguyen <vu.nguyen101@gmail.com>
2021-12-01 21:04:36 +0200

    validate email and passwrd

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
-import authRouter from './routes/auth'
+import authRoutes from './routes/auth'
 
 dotenv.config()
 
@@ -14,5 +14,6 @@ app.use(express.json())
 app.get('/ping', (_req, res) => {
   res.send('pong!')
 })
+app.use('/auth', authRoutes)
 
 export default app
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,29 @@
 import express from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
+
+router.post(
+  '/signup',
+  body('email').isEmail().withMessage('invalid email'),
+  body('password')
+    .isLength({ min: 5 })
+    .withMessage('password must be at least 5 characters'),
+  async (req, res) => {
+    const validationError = validationResult(req)
+
+    if (!validationError.isEmpty()) {
+      const errors = validationError.array().map(err => {
+        return {
+          msg: err.msg,
+        }
+      })
+      return res.status(401).json({ errors })
+    }
+
+    const { email, password } = req.body
+    res.json({ email, password })
+  },
+)
 
 export default router
